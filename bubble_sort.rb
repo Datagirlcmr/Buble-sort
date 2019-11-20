1115bbd8d2029051ef788420405bce56a8c21530
Amadou Ibrahim <amadou140589@gmail.com>
2019-11-21 00:38:49 +0100

    Use of the while loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,18 @@
 def bubble_sort(arr)
     n = arr.length
-    for i in 1..n-1 do
-        if arr[i-1] > arr[i]
-            puts arr[i], arr[i-1]
+    swapped =true
+    while swapped do
+        swapped=false
+        for i in 1..n-1 do
+            if arr[i-1] > arr[i]
+                arr[i], arr[i-1]= arr[i-1], arr[i]
+                swapped =true
+            end
         end
     end
+
+    print arr
 end
 
 
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4,10,1,5])
