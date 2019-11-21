c55a50b0e99fd06cc30d8498601b5a34ff374f5c
Selma Datagirl <datagirlcmr@gmail.com>
2019-11-21 10:28:47 -0600

    worked on the bubble_sort_by algorithm

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,12 +1,20 @@
 def bubble_sort_by(arr)
-    yield
+    n = arr.length
+    swapped =true
+    while swapped do
+        swapped=false
+        for i in 1..n-1 do
+            space = yield(arr[i - 1], arr[i])
+                if space > 0
+                    arr[i-1], arr[i] = arr[i], arr[i-1]
+                    swapped =true
+                end
+        end
+    end
+    print arr
 end
+   
 
 bubble_sort_by(["hi","hello","hey"]) do |a, b|
-    space = a.length - b.length
-    if space <= 0
-        return arr[a], arr[b]
-    else
-        return arr[b], arr[a]
-    end
-end    
+     a.length - b.length
+end
