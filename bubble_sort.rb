b0d024a8b4d049edfd6901f29eb59a006e03d87a
Amadou Ibrahim <amadou140589@gmail.com>
2019-11-21 17:59:02 +0100

    Stickler installed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
-    n = arr.length
-    swapped =true
-    while swapped do
-        swapped=false
-        for i in 1..n-1 do
-            if arr[i-1] > arr[i]
-                arr[i], arr[i-1]= arr[i-1], arr[i]
-                swapped =true
-            end
-        end
+  n = arr.length
+  swapped = true
+  while swapped
+    swapped = false
+    (1..n - 1).each do |i|
+      if arr[i - 1] > arr[i]
+        arr[i], arr[i - 1] = arr[i - 1], arr[i]
+        swapped = true
+      end
     end
-    print arr
+  end
+  arr
 end
 
-
-bubble_sort([4,10,1,5])
-
+p bubble_sort([4, 10, 1, 5])
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,20 +1,23 @@
-def bubble_sort_by(arr)
-    n = arr.length
-    swapped =true
-    while swapped do
-        swapped=false
-        for i in 1..n-1 do
-            space = yield(arr[i - 1], arr[i])
-                if space > 0
-                    arr[i-1], arr[i] = arr[i], arr[i-1]
-                    swapped =true
-                end
-        end
-    end
-    print arr
-end
-   
+# frozen_string_literal: true
 
-bubble_sort_by(["hi","hello","hey"]) do |a, b|
-     a.length - b.length
+def bubble_sort_by(arr)
+  n = arr.length
+  swapped = true
+  while swapped
+    swapped = false
+    (1..n - 1).each do |i|
+      space = yield(arr[i - 1], arr[i])
+      if space.positive?
+        arr[i - 1], arr[i] = arr[i], arr[i - 1]
+        swapped = true
+      end
+    end
+  end
+  arr
 end
+
+result = bubble_sort_by(%w[hi hello hey]) do |a, b|
+  a.length - b.length
+end
+
+p result
